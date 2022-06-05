e5deff418c99b4dcb906265b6a6119e5ffe1d47e
Luis Fernandez <virgoforever_4@hotmail.com>
2022-06-04 20:28:29 -0500

    fernandez1

--- app/src/main/java/edu/pe/idat/proyectomovil/LoginActivity.kt
+++ app/src/main/java/edu/pe/idat/proyectomovil/LoginActivity.kt
@@ -1,11 +1,42 @@
 package edu.pe.idat.proyectomovil
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import edu.pe.idat.proyectomovil.databinding.ActivityMainBinding
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ///setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btningresar.setOnClickListener(this)
+        binding.btnregistrarse.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.btningresar -> irMenu()
+            R.id.btnregistrarse -> irRegistrarse()
+
+        }
+
+    }
+
+    private fun irRegistrarse() {
+        val intent = Intent(this,
+            RegistroActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun irMenu() {
+        val intent = Intent(this,
+            MenuActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/edu/pe/idat/proyectomovil/MenuActivity.kt
+++ app/src/main/java/edu/pe/idat/proyectomovil/MenuActivity.kt
@@ -1,11 +1,44 @@
 package edu.pe.idat.proyectomovil
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import edu.pe.idat.proyectomovil.databinding.ActivityMainBinding
+import edu.pe.idat.proyectomovil.databinding.ActivityMenuBinding
 
-class MenuActivity : AppCompatActivity() {
+class MenuActivity : AppCompatActivity(),View.OnClickListener {
+    private lateinit var binding: ActivityMenuBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
+        ///setContentView(R.layout.activity_main)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnloguiarse.setOnClickListener(this)
+        binding.btnCarrito.setOnClickListener(this)
     }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.btnloguiarse -> irLogin()
+            R.id.btnCarrito -> irCarrito()
+
+        }
+    }
+
+    private fun irCarrito() {
+        Toast.makeText(applicationContext, "En desarrollo",Toast.LENGTH_LONG).show()
+    }
+
+    private fun irLogin() {
+
+        val intent = Intent(this,
+            LoginActivity::class.java)
+        startActivity(intent)
+    }
+
 }
