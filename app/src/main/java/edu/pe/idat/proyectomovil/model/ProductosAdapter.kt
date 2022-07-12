4d837ec4fc3f8839b4878988c820eef2502af80e
Geope81 <geope.2981@gmail.com>
2022-07-11 22:46:11 -0500

    corregido

--- app/src/main/java/edu/pe/idat/proyectomovil/model/ProductosAdapter.kt
+++ app/src/main/java/edu/pe/idat/proyectomovil/model/ProductosAdapter.kt
@@ -1,13 +1,8 @@
 package edu.pe.idat.proyectomovil.model
 
-<<<<<<< HEAD
 import android.content.Context
 import android.graphics.drawable.Drawable
 import android.net.Uri
-
-=======
-import android.net.Uri
->>>>>>> cb4d0204ae8d942153b2aecae64931eb255bd8af
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -22,17 +17,10 @@ class ProductosAdapter (val listaprod:List<Producto>):RecyclerView.Adapter<Produ
     class ProductoHolder(val view:View) :RecyclerView.ViewHolder(view){
         fun render(producto: Producto){
 
-<<<<<<< HEAD
-
-            val cadena: String="c"+producto.codproducto.toString()
-           //view.imgProducto.setImageResource(R.drawable.brasa_familiar_3)
-            view.imgProducto.setImageURI(
-               Uri.parse("android.resource://edu.pe.idat.proyectomovil/drawable/"+cadena))
-=======
             val cadena: String="img"+producto.codproducto.toString()
             view.imgProducto.setImageURI(
                 Uri.parse("android.resource://edu.pe.idat.proyectomovil/drawable/"+cadena))
->>>>>>> cb4d0204ae8d942153b2aecae64931eb255bd8af
+
 
             view.txtNombre.text = producto.nombre
             view.txtDescripcion.text = producto.descripcion
